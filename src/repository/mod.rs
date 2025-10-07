2e835dead404be20106c76d8f984c60c02d52f92
ZhenYi <434836402@qq.com>
2025-10-07 20:40:07 +0800

    update: modify repository module implementation

--- src/repository/mod.rs
+++ src/repository/mod.rs
@@ -1,13 +1,15 @@
+use std::sync::Arc;
 use crate::odb::Odb;
 use crate::refs::RefsManager;
 use crate::sha::HashVersion;
 use uuid::Uuid;
 
+#[derive(Clone)]
 pub struct Repository {
     pub id: Uuid,
     pub default_branch: String,
-    pub odb: Box<dyn Odb>,
-    pub refs: Box<dyn RefsManager>,
+    pub odb: Arc<Box<dyn Odb>>,
+    pub refs: Arc<Box<dyn RefsManager>>,
     pub hash_version: HashVersion,
 }
 
