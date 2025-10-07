9d5ad9d08eb3e60089579dbb4643db83525c6bf3
ZhenYi <434836402@qq.com>
2025-10-07 20:41:36 +0800

    update: modify transaction module implementation

--- src/transaction/mod.rs
+++ src/transaction/mod.rs
@@ -1,65 +1,29 @@
+pub mod receive;
+pub mod upload;
+pub mod advertise;
+pub mod service;
+pub mod version;
+
+
 use crate::repository::Repository;
-use crate::transaction::receive_pack::report_status::ReportStatus;
-use serde::{Deserialize, Serialize};
+use crate::callback::CallBack;
+pub(crate) use crate::transaction::service::TransactionService;
+pub(crate) use crate::transaction::version::GitProtoVersion;
 
-#[derive(Deserialize, Serialize, Clone, Debug)]
-pub enum TransactionService {
-    #[serde(rename = "git-upload-pack")]
-    UploadPack,
-    #[serde(rename = "git-receive-pack")]
-    ReceivePack,
-    #[serde(rename = "git-upload-pack-ls")]
-    UploadPackLs,
-    #[serde(rename = "git-receive-pack-ls")]
-    ReceivePackLs,
-}
 
-#[derive(Debug, Clone)]
-pub enum GitProtoVersion {
-    V0,
-    V1,
-    V2,
-}
-
+#[derive(Clone)]
 pub struct Transaction {
     pub service: TransactionService,
     pub repository: Repository,
     pub version: GitProtoVersion,
-    pub report_status: ReportStatus,
+    pub call_back: CallBack,
+    pub protocol: ProtocolType,
 }
 
-pub mod receive_pack;
-pub mod refs;
-pub mod upload_pack;
 
-impl TransactionService {
-    pub fn from_string(s: &str) -> Option<TransactionService> {
-        match s {
-            "git-upload-pack" => Some(TransactionService::UploadPack),
-            "git-receive-pack" => Some(TransactionService::ReceivePack),
-            "git-upload-pack-ls" => Some(TransactionService::UploadPackLs),
-            "git-receive-pack-ls" => Some(TransactionService::ReceivePackLs),
-            _ => None,
-        }
-    }
-    pub fn to_string(&self) -> &'static str {
-        match self {
-            TransactionService::UploadPack => "git-upload-pack",
-            TransactionService::ReceivePack => "git-receive-pack",
-            TransactionService::UploadPackLs => "git-upload-pack-ls",
-            TransactionService::ReceivePackLs => "git-receive-pack-ls",
-        }
-    }
-    pub fn is_ls(&self) -> bool {
-        match self {
-            TransactionService::UploadPackLs | TransactionService::ReceivePackLs => true,
-            _ => false,
-        }
-    }
-    pub fn is_pack(&self) -> bool {
-        match self {
-            TransactionService::UploadPack | TransactionService::ReceivePack => true,
-            _ => false,
-        }
-    }
+#[derive(Clone)]
+pub enum ProtocolType {
+    Git,
+    SSH,
+    Http,
 }
